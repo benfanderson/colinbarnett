70b67cd00cbdb58e0b2a5b4b343ef169d8f42ad4
Ben Anderson <ben.anderson@wanews.com.au>
2018-05-25 12:35:31 +0800

    Added accordion styles for different media queries

--- accordion.js
+++ accordion.js
@@ -1,26 +1,22 @@
 function init() {
-    const accordion = document.getElementsByClassName("accordion-pane");
-    const button = document.getElementsByTagName("button");
-    for (var i = 0; i < button.length; i++) {
-        // Sets accordion quotes as shut when page loads
-        accordion[i].children[1].style.display = "none";
-        //Attaches function to button
-        button[i].onclick = toggleAccordion;
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+            /* Toggle between adding and removing the "active" class,
+            to highlight the button that controls the panel */
+            this.classList.toggle("active");
+
+            /* Toggle between hiding and showing the active panel */
+            var panel = this.nextElementSibling;
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+        });
     }
 }
-
-function toggleAccordion(event) {
-    const display = event.target.parentElement.children;
-    // Toggles accordion quote open when button is clicked
-    if (display[1].style.display == "none") {
-        display[1].style.display = "block";
-        display[2].innerHTML = "Hide";
-    // Toggles accordion quote shut when button is clicked
-    } else {
-        display[1].style.display = "none";
-        display[2].innerHTML = "Show";
-    }
-}
-
 
 window.onload = init;
