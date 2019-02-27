1386cb3b4afa67f621e3f631e3b38e27902598ff
Ben Anderson <ben.anderson@wanews.com.au>
2019-02-27 16:06:33 +0800

    Added header text fade to desktop

--- accordion.js
+++ accordion.js
@@ -1,19 +1,26 @@
 function init() {
     var acc = document.getElementsByClassName("accordion");
-var i;
+  var i;
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight){
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight){
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      } 
+    });
+  }
+
+  $(window).scroll(function(){
+    $(".top").css("opacity", 1 - $(window).scrollTop() / 320);
   });
-}
-    
+
+  
 }
 
 window.onload = init;
+
+
