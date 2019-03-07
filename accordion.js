7ae2bed666ef2abfa1634097051a4e1dc321d266
Ben Anderson <ben.anderson@wanews.com.au>
2019-03-07 14:09:41 +0800

    Converted text fade JQuery function to vanilla Javascript.

--- accordion.js
+++ accordion.js
@@ -1,5 +1,13 @@
-function init() {
-    var acc = document.getElementsByClassName("accordion");
+window.onload = function() {
+
+  // Makes headline, subhead, byline fade away when scrolling
+  document.addEventListener("scroll", function(){
+    document.querySelector(".top").style.opacity = 1 - document.body.scrollTop / 100;
+    console.log(document.body.scrollTop);
+  });
+
+  // Handles quotes accordion element
+  var acc = document.getElementsByClassName("accordion");
   var i;
 
   for (i = 0; i < acc.length; i++) {
@@ -14,27 +22,7 @@ function init() {
     });
   }
 
-
-  
-
-  $(window).scroll(function(){
-    if (window.matchMedia("(min-width: 479px)")) {
-      $(".top").css("opacity", 1 - $(window).scrollTop() / 50);
-    } if ((window.matchMedia("(min-width: 1000px)"))) {
-      $(".top").css("opacity", 1 - $(window).scrollTop() / 100);
-    } else {
-      $(".top").css("opacity", 1 - $(window).scrollTop() / 320);
-    }
-    
-  });
-
-  // $(window).scroll(function(){
-  //   $(".top").css("opacity", 1 - $(window).scrollTop() / 320);
-  // });
-
-  
 }
 
-window.onload = init;
 
 
