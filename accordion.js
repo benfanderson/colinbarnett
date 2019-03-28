9a8bcc92cba3d17b4b40ed865db61781847ff4aa
Ben Anderson <ben.anderson@wanews.com.au>
2019-03-28 20:00:13 +0800

    started adding self closing accordion panel script

--- accordion.js
+++ accordion.js
@@ -1,16 +1,28 @@
 function init() {
-    var acc = document.getElementsByClassName("accordion");
-  var i;
+    let acc = document.getElementsByClassName("accordion");
+    let activePanel;
 
-  for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-      this.classList.toggle("active");
-      var panel = this.nextElementSibling;
-      if (panel.style.maxHeight){
-        panel.style.maxHeight = null;
-      } else {
-        panel.style.maxHeight = panel.scrollHeight + "px";
-      } 
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        let panel = this.nextElementSibling;
+        if (panel.style.maxHeight){
+          panel.style.maxHeight = null;
+        } else {
+          panel.style.maxHeight = panel.scrollHeight + "px";
+        } 
+        if(typeof activePanel ==="object" && typeof activePanel.nextElementSibling.style.maxHeight === "string") {
+          console.log("Boom!")
+          console.log(activePanel.nextElementSibling.style.maxHeight);
+          activePanel.classList.toggle("active");
+          let panel = activePanel.nextElementSibling;
+          if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+          } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+          } 
+        };
+        activePanel = this;
     });
   }
 
