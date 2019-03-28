b6405b4e6e6767937e28a5e2b7779d7ed5ca423a
Ben Anderson <ben.anderson@wanews.com.au>
2019-03-28 19:39:39 +0800

    Simplified JS to fade headline

--- accordion.js
+++ accordion.js
@@ -15,18 +15,21 @@ function init() {
   }
 
 
-  
-
-  $(window).scroll(function(){
-    if (window.matchMedia("(min-width: 479px)")) {
-      $(".top").css("opacity", 1 - $(window).scrollTop() / 50);
-    } if ((window.matchMedia("(min-width: 1000px)"))) {
-      $(".top").css("opacity", 1 - $(window).scrollTop() / 100);
-    } else {
-      $(".top").css("opacity", 1 - $(window).scrollTop() / 320);
-    }
-    
+  // Makes headline, subhead, byline fade away when scrolling
+  document.addEventListener("scroll", function(){
+    document.querySelector(".top").style.opacity = 1 - document.body.scrollTop / 100;
   });
+
+  // $(window).scroll(function(){
+  //   if (window.matchMedia("(min-width: 479px)")) {
+  //     $(".top").css("opacity", 1 - $(window).scrollTop() / 50);
+  //   } if ((window.matchMedia("(min-width: 1000px)"))) {
+  //     $(".top").css("opacity", 1 - $(window).scrollTop() / 100);
+  //   } else {
+  //     $(".top").css("opacity", 1 - $(window).scrollTop() / 320);
+  //   }
+    
+  // });
 
   // $(window).scroll(function(){
   //   $(".top").css("opacity", 1 - $(window).scrollTop() / 320);
