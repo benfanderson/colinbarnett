8d0a76ae8cc7c75398da0615706ee9009fa848b1
Ben Anderson <ben.anderson@wanews.com.au>
2019-12-18 15:46:13 +0800

    Linted JS file and added .gitignore

--- accordion.js
+++ accordion.js
@@ -1,32 +1,28 @@
-window.onload = function() {
-  //Handles accordion panel quote box
-  let acc = document.getElementsByClassName("accordion");
+window.onload = function () {
+  // Handles accordion panel quote box
+  const acc = document.getElementsByClassName('accordion');
   let activeButton;
 
-  for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-      this.classList.toggle("active");
-      let panel = this.nextElementSibling;
-      if (panel.style.maxHeight){
+  for (let i = 0; i < acc.length; i++) {
+    acc[i].addEventListener('click', function () {
+      this.classList.toggle('active');
+      const panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
         panel.style.maxHeight = null;
       } else {
-        panel.style.maxHeight = panel.scrollHeight + "px";
-      } 
-      if(typeof activeButton ==="object" && activeButton != this) {
-        activeButton.classList.remove("active");
+        panel.style.maxHeight = `${panel.scrollHeight}px`;
+      }
+      if (typeof activeButton === 'object' && activeButton !== this) {
+        activeButton.classList.remove('active');
         activeButton.nextElementSibling.style.maxHeight = null;
-      };
+      }
       activeButton = this;
     });
   }
 
 
   // Makes headline, subhead, byline fade away when scrolling
-  document.addEventListener("scroll", function(){
-    document.querySelector(".top").style.opacity = 1 - document.body.scrollTop / 100;
+  document.addEventListener('scroll', () => {
+    document.querySelector('.top').style.opacity = 1 - document.body.scrollTop / 100;
   });
-}
-
-
-
-
+};
